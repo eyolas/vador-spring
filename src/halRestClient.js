8e3be91b79ebca906fe8d10c07230a92e16801d7
eyolas <dtouzet@gmail.com>
2015-06-03 15:16:32 +0200

    update vador

--- src/halRestClient.js
+++ src/halRestClient.js
@@ -3,18 +3,7 @@ import {HalResource} from './halResource';
 import assign from 'lodash/object/assign';
 
 export class HalRestClient extends RestClient {
-  resource(resourceName, config = {}) {
-    if (!this._cache[resourceName]) {
-      let conf = assign({}, this._config, config);
-      conf.defaultHeaders = assign({}, this._headers, config.defaultHeaders || {});
-      conf.interceptors = (config.interceptors || []).concat(this._interceptors);
-      if (!conf.http) {
-        conf.http = this._http;
-      }
-
-      this._cache[resourceName] = new HalResource(this._baseUrl, resourceName , conf);
-    }
-
-    return this._cache[resourceName];
+  instanciateResource(resourceName, conf) {
+   return new HalResource(this._baseUrl, resourceName , conf);
   }
 }
